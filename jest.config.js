e6c03bcbf9f56b9a674a8489eaca7f0910b3608b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 12:48:54 +0700

    Build with `@un-z/project-config`

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,5 @@
-export default {
-  preset: 'ts-jest/presets/default-esm',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.spec.ts',
-    '!src/**/index.ts',
-    '!**/node_modules/**',
-  ],
-  coverageDirectory: 'target/coverage',
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
-    },
-  },
-  extensionsToTreatAsEsm: ['.ts'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: '@frontmeans/dom-primitives',
-        outputDirectory: './target/test-results',
-        classNameTemplate: '{classname}: {title}',
-        titleTemplate: '{classname}: {title}',
-        ancestorSeparator: ' › ',
-        usePathForSuiteName: 'true',
-      },
-    ],
-  ],
+import { configureJest } from '@run-z/project-config';
+
+export default await configureJest({
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.spec.json',
-        useESM: true,
-      },
-    ],
-  },
-};
+});
